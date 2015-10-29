0cc430ac2d9234ee5f99b3129e0499422c5c677b
zvacet <ikoli@yahoo.com>
2015-10-29 13:46:03 +0100

    Create lxqt-config-cursor_hr.ts

---
+++ liblxqt-config-cursor/translations/lxqt-config-cursor_hr.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>SelectWnd</name>
+    <message>
+        <location filename="../ui/selectwnd.ui" line="14"/>
+        <source>LXQt Mouse Theme Configuration</source>
+        <translation type="unfinished">Konfiguracija LXQt teme miša</translation>
+    </message>
+    <message>
+        <location filename="../ui/selectwnd.ui" line="20"/>
+        <source>Select the cursor theme you want to use (hover preview to test cursor). &lt;b&gt;LXQt session needs restart after this change&lt;/b&gt;:</source>
+        <translation type="unfinished">Odaberite temu pokazivača koju želite koristiti (pređite preko pregleda da biste testirali pokazivač). &lt;b&gt;LXQt sesiju morate ponovno pokrenuti nakon ove promjene&lt;/b&gt;:</translation>
+    </message>
+    <message>
+        <location filename="../ui/selectwnd.ui" line="81"/>
+        <source>&amp;Install New Theme...</source>
+        <translation type="unfinished">&amp;Instaliraj novu temu...</translation>
+    </message>
+    <message>
+        <location filename="../ui/selectwnd.ui" line="88"/>
+        <source>&amp;Remove Theme</source>
+        <translation type="unfinished">&amp;Ukloni-temu</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="208"/>
+        <source>XCurTheme error</source>
+        <translation type="unfinished">XCurTheme greška</translation>
+    </message>
+    <message>
+        <location filename="../selectwnd.cpp" line="209"/>
+        <source>You can&apos;t remove active theme!</source>
+        <translation type="unfinished">Ne možete ukoniti aktivnu temu!</translation>
+    </message>
+</context>
+<context>
+    <name>WarningLabel</name>
+    <message>
+        <location filename="../ui/warninglabel.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished">Forma</translation>
+    </message>
+    <message>
+        <location filename="../ui/warninglabel.ui" line="44"/>
+        <source>LXQt could not find any cursor theme. The default X11 cursor theme will be used instead. LXQt looked in the following directories:</source>
+        <translation type="unfinished">LXQt ne može naći nijednu temu pokazivača. Biti će korištena zadana X11 tema pokazivača.LXQt je gledao u slijedećedirektorije:     </translation>
+    </message>
+    <message>
+        <location filename="../ui/warninglabel.ui" line="54"/>
+        <source>Show...</source>
+        <translation type="unfinished">Pokaži...</translation>
+    </message>
+</context>
+<context>
+    <name>XCursorThemeModel</name>
+    <message>
+        <location filename="../thememodel.cpp" line="61"/>
+        <source>Name</source>
+        <translation type="unfinished">Ime</translation>
+    </message>
+    <message>
+        <location filename="../thememodel.cpp" line="62"/>
+        <source>Description</source>
+        <translation type="unfinished">Opis</translation>
+    </message>
+</context>
+</TS>
