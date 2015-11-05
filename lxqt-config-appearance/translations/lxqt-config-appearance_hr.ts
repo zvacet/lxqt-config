63223376adc951283ef10307dadd6e6f8d9d3ce5
zvacet <ikoli@yahoo.com>
2015-11-05 22:56:21 +0100

    Create lxqt-config-appearance_hr.ts

---
+++ lxqt-config-appearance/translations/lxqt-config-appearance_hr.ts
@@ -0,0 +1,225 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr">
+<context>
+    <name>FontsConfig</name>
+    <message>
+        <location filename="../fontsconfig.ui" line="23"/>
+        <source>Font</source>
+        <translation>Font</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="30"/>
+        <source>Default font for user interface</source>
+        <translation>Zadani font za korisničko sučelje</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="36"/>
+        <source>Font name:</source>
+        <translation>Ime fonta:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="46"/>
+        <source>Style:</source>
+        <translation>Stil:</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="53"/>
+        <source>Point size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="64"/>
+        <source>Normal</source>
+        <translation>Normalno</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="69"/>
+        <source>Bold</source>
+        <translation>Podebljano</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="74"/>
+        <source>Italic</source>
+        <translation>Kurziv</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="79"/>
+        <source>Bold Italic</source>
+        <translation>Podebljani kurziv</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="90"/>
+        <source>The following settings only affect newly started applications</source>
+        <translation>Slijedeće postavke imaju učinak samo na novopokrenute programe</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="96"/>
+        <source>Use antialias fonts</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="103"/>
+        <source>Font hinting style:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="114"/>
+        <location filename="../fontsconfig.ui" line="179"/>
+        <source>None</source>
+        <translation>Nijedan</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="119"/>
+        <source>Slight</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="124"/>
+        <source>Medium</source>
+        <translation>Srednje</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="129"/>
+        <source>Full</source>
+        <translation>Sav</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="137"/>
+        <source>Font hinting</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="144"/>
+        <source>Resolution (DPI):</source>
+        <translation>Razlučivost (DPI):</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="151"/>
+        <source>Autohint</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="168"/>
+        <source>Subpixel antialiasing:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="184"/>
+        <source>RGB</source>
+        <translation>RGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="189"/>
+        <source>BGR</source>
+        <translation>BGR</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="194"/>
+        <source>VRGB</source>
+        <translation>VRGB</translation>
+    </message>
+    <message>
+        <location filename="../fontsconfig.ui" line="199"/>
+        <source>VBGR</source>
+        <translation>VBGR</translation>
+    </message>
+</context>
+<context>
+    <name>IconThemeConfig</name>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="14"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>Konfiguracija LXQt izgleda</translation>
+    </message>
+    <message>
+        <location filename="../iconthemeconfig.ui" line="26"/>
+        <source>Icons Theme</source>
+        <translation>Tema ikona</translation>
+    </message>
+</context>
+<context>
+    <name>LxQtThemeConfig</name>
+    <message>
+        <location filename="../lxqtthemeconfig.ui" line="23"/>
+        <source>LXQt Theme</source>
+        <translation>LXQt tema</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="45"/>
+        <source>LXQt Appearance Configuration</source>
+        <translation>Konfiguracija LXQt izgleda</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="51"/>
+        <source>Widget Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="55"/>
+        <source>Icons Theme</source>
+        <translation>Tema ikona</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="59"/>
+        <source>LXQt Theme</source>
+        <translation>LXQt tema</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="63"/>
+        <source>Font</source>
+        <translation>Font</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="68"/>
+        <source>Cursor</source>
+        <translation>Pokazivač</translation>
+    </message>
+</context>
+<context>
+    <name>StyleConfig</name>
+    <message>
+        <location filename="../styleconfig.ui" line="23"/>
+        <source>Widget Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="54"/>
+        <source>Toolbar button style:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="62"/>
+        <source>Only display the icon</source>
+        <translation>Prikaži samo ikonu</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="67"/>
+        <source>Only display the text</source>
+        <translation>Prikaži samo tekst</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="72"/>
+        <source>The text appears beside the icon</source>
+        <translation>Tekst se pojavljuje pored ikone</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="77"/>
+        <source>The text appears under the icon</source>
+        <translation>Tekst se pojavljuje ispod ikone</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="82"/>
+        <source>Default</source>
+        <translation>Zadano</translation>
+    </message>
+    <message>
+        <location filename="../styleconfig.ui" line="90"/>
+        <source>Activate item on single click</source>
+        <translation>Aktiviraj stavku jednim klikom</translation>
+    </message>
+</context>
+</TS>
