f89246e6e024371f121725c1277c12d71480e1f4
zvacet <ikoli@yahoo.com>
2015-11-05 23:04:07 +0100

    Create lxqt-config-monitor_hr.ts

---
+++ lxqt-config-monitor/translations/lxqt-config-monitor_hr.ts
@@ -0,0 +1,220 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="hr_HR">
+<context>
+    <name>MonitorInfo</name>
+    <message>
+        <location filename="../monitor.cpp" line="54"/>
+        <source>Laptop LCD Monitor</source>
+        <translation>Laptop LCD monitor</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="56"/>
+        <source>External VGA Monitor</source>
+        <translation>Vanjski VGA monitor</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="56"/>
+        <source>VGA Monitor</source>
+        <translation>VGA monitor</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="58"/>
+        <source>External DVI Monitor</source>
+        <translation>External DVI monitor</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="58"/>
+        <source>DVI Monitor</source>
+        <translation>DVI monitor</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="60"/>
+        <source>TV</source>
+        <translation>TV</translation>
+    </message>
+    <message>
+        <location filename="../monitor.cpp" line="62"/>
+        <source>Default Monitor</source>
+        <translation>Zadani monitor</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorPictureDialog</name>
+    <message>
+        <location filename="../monitorpicture.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dijalog</translation>
+    </message>
+    <message>
+        <location filename="../monitorpicture.ui" line="27"/>
+        <source>Magnetic attraction</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MonitorSettingsDialog</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Monitor Settings</source>
+        <translation>Postavke monitora</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="29"/>
+        <source>Quick Options</source>
+        <translation>Brze opcije</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="35"/>
+        <source>Show the same screen on both laptop LCD and external monitor</source>
+        <translation>Pokaži isti zaslona nalaptop LCD-u i vanjskom monitoru</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="42"/>
+        <source>Extended view</source>
+        <translation>Prošireni pogled</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="49"/>
+        <source>Turn off laptop LCD and use external monitor only</source>
+        <translation>Isključi laptop LCD i koristi isključivo vanjski monitor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="56"/>
+        <source>Turn off external monitor and use laptop LCD only</source>
+        <translation>Iskjluči vanjski monitor i koristi samolaptop LCD</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="77"/>
+        <source>Settings</source>
+        <translation>Postavke</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="112"/>
+        <source>Unify all monitors</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="119"/>
+        <source>Monitor Position</source>
+        <translation>Pozicija monitora</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="139"/>
+        <source>Primary monitor:</source>
+        <translation>Primarni monitor:</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="266"/>
+        <source>Do you want to save changes?</source>
+        <translation>Želite li spremiti izmjene?</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="267"/>
+        <source>Please, check your config before to save.</source>
+        <translation>MOlim, provjerite vašu konfiguraciju prije spremanja.</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="293"/>
+        <source>Error</source>
+        <translation>Greška</translation>
+    </message>
+    <message>
+        <location filename="../monitorsettingsdialog.cpp" line="293"/>
+        <source>Config can not be saved</source>
+        <translation>Konfiguraciju nije moguće spremiti</translation>
+    </message>
+</context>
+<context>
+    <name>MonitorWidget</name>
+    <message>
+        <location filename="../monitorwidget.ui" line="23"/>
+        <source>Turn on</source>
+        <translation>Uključi</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="30"/>
+        <source>Resolution:</source>
+        <translation>Razlučivost:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="40"/>
+        <source>Rate:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="58"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="67"/>
+        <source>Position:</source>
+        <translation>Pozicija:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="79"/>
+        <source>x:</source>
+        <translation>x:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="96"/>
+        <source>y:</source>
+        <translation>y:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="115"/>
+        <source>Brightness:</source>
+        <translation>Svjetlina:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="135"/>
+        <source>Gamma:</source>
+        <translation>Gamma:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="154"/>
+        <location filename="../monitorwidget.ui" line="171"/>
+        <source>:</source>
+        <translation>:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.ui" line="193"/>
+        <source>Backlight:</source>
+        <translation>Pozadinsko svijetlo:</translation>
+    </message>
+    <message>
+        <location filename="../monitorwidget.cpp" line="46"/>
+        <location filename="../monitorwidget.cpp" line="97"/>
+        <source>Auto</source>
+        <translation type="unfinished">Auto</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../xrandr.cpp" line="238"/>
+        <location filename="../xrandr.cpp" line="243"/>
+        <source>Auto</source>
+        <translation>Auto</translation>
+    </message>
+</context>
+<context>
+    <name>TimeoutDialog</name>
+    <message>
+        <location filename="../timeoutdialog.ui" line="14"/>
+        <source>Settings are changed</source>
+        <translation>Postavke su promjenjene</translation>
+    </message>
+    <message>
+        <location filename="../timeoutdialog.ui" line="23"/>
+        <source>Are the current settings OK for you?</source>
+        <translation>Jesu li trenutne postavke u redu za vas?</translation>
+    </message>
+    <message>
+        <location filename="../timeoutdialog.cpp" line="52"/>
+        <source>%1 second(s) remaining</source>
+        <translation>%1 sekunda(I) preostalo</translation>
+    </message>
+</context>
+</TS>
